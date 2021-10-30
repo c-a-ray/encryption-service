a5dd9433b45f1619ccdd38fcd98e6f94cb61e893
c-a-ray <c.a.ray@outlook.com>
2021-10-30 16:23:46 -0700

    Squash bugs

--- credentials.py
+++ credentials.py
@@ -14,16 +14,16 @@ class Credentials:
         self.password = args.password
         self.text_state = self.get_text_state(args)
 
-        self.set_and_validate_output_dir()
+        self.set_and_validate_output_dir(args)
 
         if not self.has_error():
-            self.get_or_create_key()
+            self.get_or_create_key(args)
 
     def get_text_state(self, args):
         if args.encrypt:
-            return ENCRYPTED
-        elif args.decrypt:
             return DECRYPTED
+        elif args.decrypt:
+            return ENCRYPTED
         else:
             return UNKOWN
 
@@ -37,6 +37,7 @@ class Credentials:
             if args.keypath:
                 if exists(args.keypath):
                     self.keypath = args.keypath
+                    self.unique_identifier = self.keypath.split('.key')[-1]
                 else:
                     self.error = f'Invalid argument: key file {args.keypath} does not exist'
             else:
@@ -61,11 +62,11 @@ class Credentials:
 
         # Generate and write new key to new key file in output dir
         with open(self.keypath, 'w') as key_file:
-            key_file.write(Fernet.generate_key())
+            key_file.write(Fernet.generate_key().decode())
 
     def load_key(self) -> str:
         with open(self.keypath, 'r') as key_file:
-            return Fernet(key_file.read())
+            return key_file.read()
 
     def encrypt(self):
         f = Fernet(self.load_key())
@@ -78,7 +79,10 @@ class Credentials:
         self.password = f.decrypt(self.password.encode()).decode()
 
     def write_to_file(self):
-        output_filename = f'credentials{self.unique_identifier}.json'
+        output_filename = f'decrypted-{self.unique_identifier}.json' \
+            if self.text_state == ENCRYPTED \
+            else f'encrypted-{self.unique_identifier}.json'
+
         with open(join(self.outputdir, output_filename), 'w') as output_file:
             output_file.write(dumps({
                 'username': self.username,
