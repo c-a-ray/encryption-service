e95ffbc623281e966243b4b873ff344f7b72edb9
c-a-ray <c.a.ray@outlook.com>
2021-10-30 15:26:13 -0700

    Add remaining arguments

--- args.py
+++ args.py
@@ -8,11 +8,17 @@ def get_args():
     m_group.add_argument('-e', '--encrypt', action='store_true',
                          help='use to encrypt credentials')
     m_group.add_argument('-d', '--decrypt', action="store_true",
-                         help='use to decrypt credentials')
+                         help='Use to decrypt credentials')
 
     parser.add_argument('-u', '--username', type=str, required=True,
-                        help='specify a username to encrypt or decrypt')
+                        help='Specify a username to encrypt or decrypt')
     parser.add_argument('-p', '--password', type=str, required=True,
-                        help='specify a password to encrypt or decrypt')
+                        help='Specify a password to encrypt or decrypt')
+
+    parser.add_argument('-k', '--keypath', type=str,
+                        help='Specify path to key file (for decryption only)')
+
+    parser.add_argument('-o', '--output', type=str,
+                        required=True, help='Specify path to output directory')
 
     return parser.parse_args()
