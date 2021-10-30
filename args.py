a1df4f66f996a7fc7d13b7db8646884c01f339dd
c-a-ray <c.a.ray@outlook.com>
2021-10-30 15:30:14 -0700

    Capitalize error message

--- args.py
+++ args.py
@@ -6,7 +6,7 @@ def get_args():
 
     m_group = parser.add_mutually_exclusive_group()
     m_group.add_argument('-e', '--encrypt', action='store_true',
-                         help='use to encrypt credentials')
+                         help='Use to encrypt credentials')
     m_group.add_argument('-d', '--decrypt', action="store_true",
                          help='Use to decrypt credentials')
 
